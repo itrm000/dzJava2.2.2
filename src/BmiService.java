02066a534ed11100eebcf773b2242964af4fdab5
Anatoly <itrm000@gmail.com>
2020-09-28 01:03:02 +0500

    Second commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,35 +1,8 @@
 public class BmiService {
     public float calculate(float growth, float weight) {
-        float index = growth/((weight * weight)/100/100); /* указываем рост в см, соответственно делим на 100  */
+        float index = growth / ((weight * weight) / 100 / 100);
 
-        return  index;
+        return index;
     }
 }
 
-//
-//
-//
-//public class BonusMilesService {
-//    public int calculate (int cost) {
-//        int miles = cost / 20;
-//
-//        return  miles;
-//    }
-//
-//}
-//
-//public class BonusService {
-//
-//    public long calculate(long amount, boolean registered) {
-//
-//        int percent = registered ? 3 : 1; /* тернарный оператор */
-//        long bonus = amount * percent/ 100 / 100;
-//        long limit = 500;
-//        if (bonus > limit) {
-//            bonus = limit;
-//        }
-//
-//        return  bonus;
-//    }
-//
-//}
