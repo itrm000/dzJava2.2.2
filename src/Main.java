2b5c4dc9addc0347fbb6f9f7e2599c472543fc2a
Anatoly <itrm000@gmail.com>
2020-12-05 23:19:31 +0500

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        float growth = 86; /* вес */
-        float weight = 178;  /* рост */
+        float growth = 72; /* вес */
+        float weight = 167;  /* рост */
         float index = service.calculate(growth, weight);
         System.out.println(index);
 
